9c133f00b3af1821b440ec7bf9777327b5a0d8f4
Eric Lau <ericlaucmo@gmail.com>
2020-04-01 22:29:14 -0700

    Dupe of https://github.com/Team-NightHawk

---
+++ server/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const router = express.Router();
+
+const { find } = require('../database-final/dbhelpers');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use('/', router);
+
+const PORT = 3001;
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server is listening on port ${PORT}`);
+  }
+});
+
+router.get(`/search/:keyword`, (req, res) => {
+  console.log('Searching!-->', req.params);
+  find(req.params)
+    .then((result) => {
+      console.log('Successful!');
+      res.status(200).send(result);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+router.get(`/count/:keyword`, (req, res) => {
+  console.log('counting!');
+
+  count(req.params)
+    .then((result) => {
+      console.log('Successful!');
+      res.status(200).send(result);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
